d5d9f5dbcb5573c8bf970ac705ef2c1ea698043b
byeongdoohan <quden04@gmail.com>
2023-01-02 09:08:21 +0900

    파일 수정

---
+++ src/main/java/com/sparta/hanghaeblog/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.sparta.hanghaeblog.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig extends WebSecurityConfiguration {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
+                // 어떤 요청이든 '인증'
+                .anyRequest().authenticated()
+                .and()
+                // 로그인 기능 허용
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/user/login?error")
+                .permitAll()
+                .and()
+                // 로그아웃 기능 허용
+                .logout()
+                .permitAll();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf().disable();
+
+        http.authorizeRequests().anyRequest().authenticated();
+
+        // 로그인 사용
+        http.formLogin();
+
+        return http.build();
+    }
+
+}
